90fc8f9865c582e01be006b67e3b844bc3386de7
Corentin Delorme <codelorme@gmail.com>
2021-12-29 07:28:04 +0100

    Add script to generate Google sitemap.txt

---
+++ scripts/sitemap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd "$(dirname "$(dirname "${0}")")"
+
+SITEMAP='sitemap.txt'
+BASEURL='https://zomboid-javadoc.com/'
+
+find 38.30 39.67 40.43 41.65 -type f -name '*.html' | sed "s#^#${BASEURL}#" > "${SITEMAP}"
+
+echo OK
