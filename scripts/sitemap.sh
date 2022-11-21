e887dfdbfd6489e32301b7b7589f19dee9bc2065
Corentin Delorme <codelorme@gmail.com>
2022-11-21 13:14:38 +0100

    Update script sitemap.sh: Add version 41.78

--- scripts/sitemap.sh
+++ scripts/sitemap.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$(dirname "${0}")")"
 SITEMAP='sitemap.txt'
 BASEURL='https://zomboid-javadoc.com/'
 
-find 38.30 39.67 40.43 41.65 -type f -name '*.html' | sed "s#^#${BASEURL}#" > "${SITEMAP}"
+find 38.30 39.67 40.43 41.65 41.78 -type f -name '*.html' | sed "s#^#${BASEURL}#" > "${SITEMAP}"
 
 echo OK
